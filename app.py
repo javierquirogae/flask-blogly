b20fe21fc0f9e6181c39d27b5032e9de272d3e16
Javier Quiroga <javier.quiroga.e@gmail.com>
2023-01-25 21:32:50 -0500

    bones

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-"""Blogly application."""
-
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, request, render_template,  redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///music'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "123"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
 
 connect_db(app)
-db.create_all()
+print('connect_db')
+
+
+
+@app.route('/')
+def home_page():
+    return render_template('home.html')
